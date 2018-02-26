ca565a92c80d9d33d948f0ceab80714a0529da36
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2018-02-26 00:34:47 -0600

    added components and images

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cards from "./cards.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.cards to the friends json array
+  state = {
+    cards
+  };
+
+  removeCard = id => {
+    // Filter this.state.Card for friends with an id not equal to the id being removed
+    const cards = this.state.cards.filter(card => card.id !== id);
+    // Set this.state.friends equal to the new cards array
+    this.setState({ cards });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div class="container-fluid">
-        <div class="container">
-          // HEADING
-          <div class="row">
-          </div>
-
-          // Cards
-          <div class="row">
-            <div class="col-3 col-md-3 col-sm-6">
-            </div>
-            <div class="col-3 col-md-3 col-sm-6">
-            </div>
-            <div class="col-3 col-md-3 col-sm-6">
-            </div>
-            <div class="col-3 col-md-3 col-sm-6">
-            </div>
-          </div>
-
-        </div>
-
-      </div>
+      <Wrapper>
+        <Title>StarWars Clicky Card Game</Title>
+        {this.state.cards.map(card => (
+          <Card
+            removeCard={this.removeCard}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+          {props.name}
+    </div>
+  </div>
+);
+
+export default Card;
